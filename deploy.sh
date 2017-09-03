8ac5785c0880d868070faddbd9cbf9ed8b44d190
Matthew Crenshaw <sgt@squig.gs>
2017-09-03 16:12:51 -0400

    fixed bg image issue

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,16 @@ DISTRIBUTION_ID=E1ZTFHJ6XDJ8NZ
 BUCKET_NAME=squig.gs
 PROFILE=mine
 
-sass --update sass:static/css
+rm -rf public
+sass --update sass:static/css --sourcemap=none --style=compressed
 hugo -v
 
-# Copy over pages - not static js/img/css/downloads
-aws s3 sync --profile ${PROFILE} --acl "public-read" --sse "AES256" public/ s3://${BUCKET_NAME}/ --exclude 'js' --exclude 'css' --exclude 'fonts'
+aws s3 sync --profile ${PROFILE} --acl "public-read" --sse "AES256" public/ s3://${BUCKET_NAME}/ --delete --exclude 'js' --exclude 'css' --exclude 'fonts' --exclude '*.DS_Store'
 
 # Ensure static files are set to cache forever - cache for a month --cache-control "max-age=2592000"
-aws s3 sync --profile ${PROFILE} --cache-control "max-age=2592000" --acl "public-read" --sse "AES256" public/fonts/ s3://${BUCKET_NAME}/fonts/
-aws s3 sync --profile ${PROFILE} --cache-control "max-age=2592000" --acl "public-read" --sse "AES256" public/css/ s3://${BUCKET_NAME}/css/
-aws s3 sync --profile ${PROFILE} --cache-control "max-age=2592000" --acl "public-read" --sse "AES256" public/js/ s3://${BUCKET_NAME}/js/
+aws s3 sync --profile ${PROFILE} --cache-control "max-age=2592000" --acl "public-read" --sse "AES256" public/fonts/ s3://${BUCKET_NAME}/fonts/ --delete --exclude '*.DS_Store'
+aws s3 sync --profile ${PROFILE} --cache-control "max-age=2592000" --acl "public-read" --sse "AES256" public/css/ s3://${BUCKET_NAME}/css/ --delete --exclude '*.DS_Store'
+aws s3 sync --profile ${PROFILE} --cache-control "max-age=2592000" --acl "public-read" --sse "AES256" public/js/ s3://${BUCKET_NAME}/js/ --delete --exclude '*.DS_Store'
 
 # Invalidate landing page so everything sees new post - warning, first 1K/mo free, then 1/2 cent each
 aws cloudfront create-invalidation --profile ${PROFILE} --distribution-id ${DISTRIBUTION_ID} --paths /index.html /
